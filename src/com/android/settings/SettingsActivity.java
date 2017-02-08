623058da578475c0bb361f497b847c2df3fef278
Shaun <shaunsibu@gmail.com>
2017-02-08 00:59:35 -0700

    Found a part that was reverted that I did not catch
    - Also fix import
    
    Change-Id: If76d4524017f9d740041ae34bb9e6a5699bdf2c6
    
    Conflicts:
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -153,7 +153,7 @@ import com.android.settings.rr.navbar.SmartbarSettings;
 import com.android.settings.rr.navbar.PulseSettings;
 import com.android.settings.rr.AppCircleBar;
 import com.android.settings.rr.pie.PieSettings;
-import com.android.settings.rr.AppSideBar;
+import com.android.settings.rr.AppSidebar;
 import com.android.settings.rr.gestureanywhere.GestureAnywhereSettings;
 import com.android.settings.rr.ambientdisplay.AmbientSettings;
 import com.android.settings.rr.Headsup;
@@ -406,7 +406,7 @@ public class SettingsActivity extends SettingsDrawerActivity
             SmartbarSettings.class.getName(),
             PulseSettings.class.getName(),
             AppCircleBar.class.getName(),
-            AppSideBar.class.getName(),
+            AppSidebar.class.getName(),
             GestureAnywhereSettings.class.getName(),
             HWSettings.class.getName(),
             PieSettings.class.getName(),
