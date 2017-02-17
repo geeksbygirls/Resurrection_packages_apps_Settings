0866cc49e344f48a9603d072bb0d4bd15be849a3
ezio84 <brabus84@gmail.com>
2017-02-17 11:30:47 -0700

    Allow to customize max SMS per minute limit [1/2]
    Aosp has a fixed 30sms per minute value, stored in the Settings.java:
           public static final String SMS_OUTGOING_CHECK_MAX_COUNT =
                   "sms_outgoing_check_max_count";
    
    and src/java/com/android/internal/telephony/SmsUsageMonitor.java
    in the frameworks/opt/telephony:
        /** Default number of SMS sent in checking period without user permission. */
        private static final int DEFAULT_SMS_MAX_COUNT = 30;
    
    Remove the pref if the device isn't a phone.
    
    The 2/2 part is in the SmsUsageMonitor class, needed to avoid a reboot to
    update the Global Setting, and it was already made by CM guys:
    ezio84/abc-n_frameworks_opt_telephony@b3be4cb
    
    Change-Id: Ifdcfe03e0089c8141f206ff5a7dcc318c919d600
    
    Conflicts:
    	res/values/pimps_arrays.xml
    	res/values/pimps_strings.xml
    	res/xml/rr_misc.xml
    	src/com/android/settings/rr/MiscSettings.java

--- src/com/android/settings/rr/MiscSettings.java
+++ src/com/android/settings/rr/MiscSettings.java
@@ -32,6 +32,7 @@ import android.support.v7.preference.ListPreference;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 import com.android.settings.util.Helpers;
@@ -55,15 +56,27 @@ public class MiscSettings extends SettingsPreferenceFragment
     private static final String APP_REMOVER = "system_app_remover";
     private static final String ROOT_ACCESS_PROPERTY = "persist.sys.root_access";
     private static final String PREF_MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
+    private static final String APPS_SECURITY = "apps_security";
+    private static final String SMS_OUTGOING_CHECK_MAX_COUNT = "sms_outgoing_check_max_count";
+
     private PreferenceScreen mAppRemover;
     private ListPreference mMsob;
+    private ListPreference mSmsCount;
+    private int mSmsCountValue;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.rr_misc);
-  	    final ContentResolver resolver = getActivity().getContentResolver();
+
+  	final ContentResolver resolver = getActivity().getContentResolver();
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        PreferenceCategory appsSecCategory = (PreferenceCategory) findPreference(APPS_SECURITY);
+
+
 
         mAppRemover = (PreferenceScreen) findPreference(APP_REMOVER);
 
@@ -72,6 +85,17 @@ public class MiscSettings extends SettingsPreferenceFragment
                 Settings.System.MEDIA_SCANNER_ON_BOOT, 0)));
         mMsob.setSummary(mMsob.getEntry());
         mMsob.setOnPreferenceChangeListener(this);
+
+        mSmsCount = (ListPreference) findPreference(SMS_OUTGOING_CHECK_MAX_COUNT);
+        mSmsCountValue = Settings.Global.getInt(resolver,
+                Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, 30);
+        mSmsCount.setValue(Integer.toString(mSmsCountValue));
+        mSmsCount.setSummary(mSmsCount.getEntry());
+        mSmsCount.setOnPreferenceChangeListener(this);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            appsSecCategory.removePreference(mSmsCount);
+            prefScreen.removePreference(appsSecCategory);
+        }
 
         // Magisk Manager
         boolean magiskSupported = false;
@@ -120,6 +144,14 @@ public class MiscSettings extends SettingsPreferenceFragment
 
             mMsob.setValue(String.valueOf(value));
             mMsob.setSummary(mMsob.getEntry());
+            return true;
+        } else if (preference == mSmsCount) {
+            mSmsCountValue = Integer.valueOf((String) value);
+            int index = mSmsCount.findIndexOfValue((String) value);
+            mSmsCount.setSummary(
+                    mSmsCount.getEntries()[index]);
+            Settings.Global.putInt(resolver,
+                    Settings.Global.SMS_OUTGOING_CHECK_MAX_COUNT, mSmsCountValue);
             return true;
         }
         return false;
