d3ae527fd56e70f807ce6a832d9e6de640984733
maxwen <max.weninger@gmail.com>
2017-02-27 21:32:35 -0700

    Move OmniJaws into its own xml
    - It will only be accessible by using the weather tile more settings button
    
    Change-Id: I3dc8e6f6b185924d402359a04620dbb1ea762a71
    
    Conflicts:
    	res/xml/rr_qs_advanced.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    	src/com/android/settings/rr/QsAdvanced.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -182,6 +182,7 @@ public class Settings extends SettingsActivity {
     public static class AppCircleBarActivity extends SettingsActivity { /* empty */ }
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
     public static class ScreenStateServiceActivity extends SettingsActivity { /* empty */ }
+    public static class OmniJawsSettingsActivity extends SettingsActivity { /* empty */ }
     public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
     public static class DataSaverSettingsActivity extends SettingsActivity { /* empty */ }
     public static class HwKeysSettingsActivity extends SettingsActivity { /* empty */ }
