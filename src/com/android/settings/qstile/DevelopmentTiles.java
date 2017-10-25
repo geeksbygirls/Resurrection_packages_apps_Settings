AICP/packages_apps_Settings

Development Settings: Add QS to toggle Show touches
Change-Id: I91aac9308ae6dd90e3da6ccdacfee95363c4aa31

src/com/android/settings/qstile/DevelopmentTiles.java
@@ -32,6 +32,7 @@ public class DevelopmentTiles {
             ShowLayout.class,
             GPUProfiling.class,
             com.android.settings.aicp.qstile.DevelopmentTiles.DisableHwOverlays.class,
+            com.android.settings.aicp.qstile.DevelopmentTiles.ShowTouches.class,
     };
     public static void setTilesEnabled(Context context, boolean enable) {
         final PackageManager pm = context.getPackageManager();

geeksbygirls/Resurrection_packages_apps_Settings

Conflicts:
	res/values/rr_strings.xml
	src/com/android/settings/qstile/DevelopmentTiles.java

src/com/android/settings/qstile/DevelopmentTiles.java
@@ -32,6 +32,7 @@ public class DevelopmentTiles {
             ShowLayout.class,
             GPUProfiling.class,
             com.android.settings.rr.qstile.DevelopmentTiles.DisableHwOverlays.class,
+            com.android.settings.rr.qstile.DevelopmentTiles.ShowTouches.class,
     };
     public static void setTilesEnabled(Context context, boolean enable) {
         final PackageManager pm = context.getPackageManager();